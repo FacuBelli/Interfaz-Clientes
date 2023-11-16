3bd120e300a49af4875a16273cd216bb158f1503
FacuBelli <facundo_carabelli@hotmail.com>
2023-11-16 20:41:50 -0300

    Saque funcion cargar datos

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def opciones():
     try:
     
-        opcion = int(input("\n1.Cargar compras\n2.Cargar ventas\n3.Generar reporte\n4.Buscar periodo Compras\n5.Buscar periodo Ventas\n6.IVA a pagar Total\n7.Consultar total de un periodo\n8.Cancelar\n¿Que accion quiere visualizar?: "))
-        
+        opcion = int(input("\n1.Cargar compras\n2.Cargar ventas\n3.Generar reporte\n4.Buscar periodo Compras\n5.Buscar periodo Ventas\n6.IVA a pagar Total\n7.Consultar total de un periodo\n8.Cancelar\n¿Que accion quiere visualizar?: "))   
         #Validacion
         while opcion <= 0 or opcion > 8:
             opcion = int(input("Opcion no correcta. Estas son las opciones posibles:\n1.Cargar compras\n2.Cargar ventas\n3.Generar reporte\n4.Buscar periodo Compras\n5.Buscar periodo Ventas\n6.IVA a pagar Total\n7.Consultar total de un periodo\n8.Cancelar\n¿Que accion quiere visualizar?: "))
@@ -264,35 +263,7 @@ def generarReporte():
     except ValueError:
         print("Error: Ingrese un dato válido.")
     opciones()
-#Opcion XX
-def cargarDatos(columnas, filas):
-    archivoDatos = 'Datos.txt'
 
-    matriz = [[0]* columnas for i in range(filas)]
-    matriz[0][0] = 'Mes'
-    matriz[0][1] = 'Año'
-    matriz[0][2] = 'IVA Compras'
-    matriz[0][3] = 'IVA Ventas'
-    matriz[0][4] = 'Diferencia'
-    with open(archivoDatos,'a') as archivo:
-        while True:
-            for c in range (1,columnas):
-                matriz[c][0] = 'agosto'#str(input('Mes: ').lower())
-                matriz[c][1] = '2023'
-                matriz[c][2] = 234 #int(input(f'IVA Compras de {matriz[c][0]}: '))
-                matriz[c][3] = 234 #int(input(f'IVA Ventas de {matriz[c][0]}: '))
-                matriz[c][4] = (matriz[c][2] - matriz[c][3])
-                #Esto escribe los datos en el txt
-                linea = ','.join(map(str, matriz[c]))
-                archivo.write(linea + '\n')
-            continuar = input("¿Desea cargar mas datos? (si/no): ")
-            continuar=continuar.lower()
-            if continuar == "si":
-                continue
-            else:
-                break
-
-        opciones()
 
 
 
